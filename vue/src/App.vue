bbf32c1da5fea77d83e122ee86fc3414ce2120b3
Richard Borges <richard.borges@contaazul.com>
2021-05-02 11:25:28 -0300

    chore: start cheatsheet component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cheatsheet />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Cheatsheet from './components/Cheatsheet.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Cheatsheet
   }
 }
 </script>
