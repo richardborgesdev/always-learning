093e873bf34152c84383883ff332a0e537200245
Richard Borges <richard.borges@contaazul.com>
2021-05-04 11:27:00 -0300

    chore: add return an error in future function

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -1,5 +1,5 @@
 void main() {
-  exercisePuttingItAllTogether();
+  completingFutureWithAnError();
 }
 
 /*
@@ -658,5 +658,21 @@ Future<void> fetchUserOrder() {
 
 void introducingFutures() {
   fetchUserOrder();
+  print('Fetching user order...');
+}
+
+/*
+  https://dart.dev/codelabs/async-await#example-completing-with-an-error
+*/
+Future<void> fetchUserOrderError() {
+// Imagine that this function is fetching user info but encounters a bug
+  return Future.delayed(
+    Duration(seconds: 2),
+    () => throw Exception('Logout failed: user ID is invalid')
+  );
+}
+
+void completingFutureWithAnError() {
+  fetchUserOrderError();
   print('Fetching user order...');
 }
