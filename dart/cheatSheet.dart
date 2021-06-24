39c54da4831e031d45b11e227b4c196e059bb2bb
Richard Borges <richard.borges@contaazul.com>
2021-06-24 09:38:58 -0300

    chore: add arithmetic operators example

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -1516,6 +1516,37 @@ void operators() {
   */
 }
 
+// https://dart.dev/guides/language/language-tour#arithmetic-operators
+void arithmeticOperators() {
+  assert(2 + 3 == 5);
+  assert(2 - 3 == -1);
+  assert(2 * 3 == 6);
+  assert(5 / 2 == 2.5); // Result is a double
+  assert(5 ~/ 2 == 2); // Result is an int
+  assert(5 % 2 == 1); // Remainder
+
+  assert('5/2 = ${5 ~/ 2} r ${5 % 2}' == '5/2 = 2 r 1');
+
+  // prefix and postfix
+  var a, b;
+
+  a = 0;
+  b = ++a; // Increment a before b gets its value.
+  assert(a == b); // 1 == 1
+
+  a = 0;
+  b = a++; // Increment a AFTER b gets its value.
+  assert(a != b); // 1 != 0
+
+  a = 0;
+  b = --a; // Decrement a before b gets its value.
+  assert(a == b); // -1 == -1
+
+  a = 0;
+  b = a--; // Decrement a AFTER b gets its value.
+  assert(a != b); // -1 != 0
+}
+
 /*
   https://dart.dev/guides/language/language-tour#the-main-function
   Run the app like this: dart args.dart 1 test
