f273b1ec90098590261ac313a6f30c9a0b1c255e
Richard Borges <richard.borges@contaazul.com>
2021-07-18 09:48:29 -0300

    chore: getter and setter examples

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -2060,6 +2060,20 @@ class Vector {
   // ···
 }
 
+// getters and setters
+class Rectangle {
+  double left, top, width, height;
+
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  // Define two calculated properties: right and bottom.
+  double get right => left + width;
+  set right(double value) => left = value - width;
+  
+  double get bottom => top + height;
+  set bottom(double value) => top = value - height;
+}
+
 void methodsExample() {
   // Operators
   final v = Vector(2, 3);
@@ -2067,6 +2081,12 @@ void methodsExample() {
 
   assert(v + w == Vector(4, 5));
   assert(v - w == Vector(0, 1));
+
+  // getters and setters
+  var rect = Rectangle(3, 4, 20, 15);
+  assert(rect.left == 3);
+  rect.right = 12;
+  assert(rect.left == -8);
 }
 
 // https://dart.dev/guides/language/language-tour#the-main-function
