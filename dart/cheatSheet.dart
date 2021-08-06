88a02473e56b9c18719ca3fde9e45c5254f6548e
Richard Borges <richard.borges@contaazul.com>
2021-08-06 13:43:41 -0300

    chore: specifying library prefix examples

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -2405,6 +2405,18 @@ void usingLibraries() {
     // Import all names EXCEPT foo.
     import 'package:lib2/lib2.dart' hide foo;
   */
+
+  // Specifying a library prefix
+  /*
+    import 'package:lib1/lib1.dart';
+    import 'package:lib2/lib2.dart' as lib2;
+
+    // Uses Element from lib1.
+    Element element1 = Element();
+
+    // Uses Element from lib2.
+    lib2.Element element2 = lib2.Element();
+  */
 }
 
 // https://dart.dev/guides/language/language-tour#the-main-function
