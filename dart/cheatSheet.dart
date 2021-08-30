bd07339dce6ba6b8206fa7ac67395ec13fd69e37
Richard Borges <richard.borges@contaazul.com>
2021-08-30 14:01:06 -0300

    chore: extract data from a string examples

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -2754,6 +2754,31 @@ void searchingInsideAString() {
   assert('Never odd or even'.indexOf('odd') == 6);
 }
 
+void extractingDataFromAString() {
+  // Grab a substring.
+  assert('Never odd or even'.substring(6, 9) == 'odd');
+
+  // Split a string using a string pattern.
+  var parts = 'structured web apps'.split(' ');
+  assert(parts.length == 3);
+  assert(parts[0] == 'structured');
+
+  // Get a UTF-16 code unit (as a string) by index.
+  assert('Never odd or even'[0] == 'N');
+
+  // Use split() with an empty string parameter to get
+  // a list of all characters (as Strings); good for
+  // iterating.
+  for (var char in 'hello'.split('')) {
+    print(char);
+  }
+
+  // Get all the UTF-16 code units in the string.
+  var codeUnitList =
+      'Never odd or even'.codeUnits.toList();
+  assert(codeUnitList[0] == 78);
+}
+
 // https://dart.dev/guides/language/language-tour#the-main-function
 // Run the app like this: dart args.dart 1 test
 void main(List<String> arguments) {
