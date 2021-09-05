41a6fa1713811163b8ac72a201664247240d6e79
Richard Borges <richard.borges@contaazul.com>
2021-09-05 13:19:36 -0300

    chore: lists examples

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -2754,6 +2754,39 @@ void searchingInsideAString() {
   assert('Never odd or even'.indexOf('odd') == 6);
 }
 
+// https://dart.dev/guides/libraries/library-tour#collections
+// Lists
+void listsExample() {
+  // Create an empty list of strings.
+  var grains = <String>[];
+  assert(grains.isEmpty);
+
+  // Create a list using a list literal.
+  var fruits = ['apples', 'oranges'];
+
+  // Add to a list.
+  fruits.add('kiwis');
+
+  // Add multiple items to a list.
+  fruits.addAll(['grapes', 'bananas']);
+
+  // Get the list length.
+  assert(fruits.length == 5);
+
+  // Remove a single item.
+  var appleIndex = fruits.indexOf('apples');
+  fruits.removeAt(appleIndex);
+  assert(fruits.length == 4);
+
+  // Remove all elements from a list.
+  fruits.clear();
+  assert(fruits.isEmpty);
+
+  // You can also create a List using one of the constructors.
+  var vegetables = List.filled(99, 'broccoli');
+  assert(vegetables.every((v) => v == 'broccoli'));
+}
+
 void extractingDataFromAString() {
   // Grab a substring.
   assert('Never odd or even'.substring(6, 9) == 'odd');
