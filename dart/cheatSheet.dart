1b6b806c36fc5800ac4154e8dcb21688626ab166
Richard Borges <richard.borges@contaazul.com>
2021-09-22 09:59:32 -0300

    chore: milliseconds since epoch example

--- dart/cheatSheet.dart
+++ dart/cheatSheet.dart
@@ -3129,6 +3129,16 @@ void creatingDates() {
   y2k = DateTime.parse('2000-01-01T00:00:00Z');
 }
 
+void getMillisecondsSinceEpoch() {
+  // 1/1/2000, UTC
+  var y2k = DateTime.utc(2000);
+  assert(y2k.millisecondsSinceEpoch == 946684800000);
+
+  // 1/1/1970, UTC
+  var unixEpoch = DateTime.utc(1970);
+  assert(unixEpoch.millisecondsSinceEpoch == 0);
+}
+
 // https://dart.dev/guides/language/language-tour#the-main-function
 // Run the app like this: dart args.dart 1 test
 void main(List<String> arguments) {
