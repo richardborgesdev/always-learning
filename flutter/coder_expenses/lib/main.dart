09ec696213d1bf05b2cec35331cf7e9e9eca0820
Richard Borges <richard.borges@contaazul.com>
2022-02-02 10:03:44 -0300

    chore: show transactions

--- flutter/coder_expenses/lib/main.dart
+++ flutter/coder_expenses/lib/main.dart
@@ -43,9 +43,12 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              child: Text('Lista de transacoes'),
-              elevation: 5,
+            Column(
+              children: _transactions
+                  .map((tr) => Card(
+                        child: Text(tr.title),
+                      ))
+                  .toList(),
             )
           ],
         ));
