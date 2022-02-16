f0c009c58ed1c08683f20c1d1a991601c366442a
Richard Borges <richard.borges@contaazul.com>
2022-02-16 13:38:32 -0300

    chore: callback to add transactions

--- flutter/coder_expenses/lib/components/transaction_form.dart
+++ flutter/coder_expenses/lib/components/transaction_form.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +32,9 @@ class TransactionForm extends StatelessWidget {
               children: [
                 TextButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                   child: Text('Nova transação'),
                   style: TextButton.styleFrom(
