e0d06d0ebe6428738385e4c0d5277182b71802ed
Richard Borges <richard.borges@contaazul.com>
2022-03-02 10:14:50 -0300

    chore: understanding theme applications

--- flutter/coder_expenses/lib/components/transaction_list.dart
+++ flutter/coder_expenses/lib/components/transaction_list.dart
@@ -24,7 +24,7 @@ class TransactionList extends StatelessWidget {
                 ),
                 decoration: BoxDecoration(
                     border: Border.all(
-                  color: Colors.purple,
+                  color: Theme.of(context).colorScheme.primary,
                   width: 2,
                 )),
                 padding: EdgeInsets.all(10),
@@ -33,7 +33,7 @@ class TransactionList extends StatelessWidget {
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 20,
-                    color: Colors.purple,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                 ),
               ),
