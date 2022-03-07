bf4acaed72b5493eb5b72547471c9fa064dfa4d0
Richard Borges <richard.borges@contaazul.com>
2022-03-07 10:32:16 -0300

    chore: create chart

---
+++ flutter/coder_expenses/lib/components/chart.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        bool isSameDay = recentTransaction[i].date.day == weekDay.day;
+        bool isSameMonth = recentTransaction[i].date.month == weekDay.month;
+        bool isSameYear = recentTransaction[i].date.year == weekDay.year;
+
+        if (isSameDay && isSameMonth && isSameYear) {
+          totalSum = recentTransaction[i].value;
+        }
+      }
+
+      Map<String, Object> chartValues = {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum
+      };
+      print(chartValues);
+
+      return chartValues;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: []),
+    );
+  }
+}
