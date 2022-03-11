aa5ef6bff2ee52267fd35b516b0de8a60190dae1
Richard Borges <richard.borges@contaazul.com>
2022-03-11 10:47:57 -0300

    chore: show chart bar values

---
+++ flutter/coder_expenses/lib/components/chart_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percent;
+
+  ChartBar({this.label = '', this.percent = 0.0, this.value = 0.0});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('R\$${value.toStringAsFixed(2)}'),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(5))),
+              FractionallySizedBox(
+                heightFactor: percent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label)
+      ],
+    );
+  }
+}
