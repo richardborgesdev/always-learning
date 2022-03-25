6c5a84151cdfd6fb8ebb43038286b9cfd32e4aff
Richard Borges <richard.borges@contaazul.com>
2022-03-25 10:00:24 -0300

    chore: add responsivity approachs

--- flutter/coder_expenses/lib/components/transaction_list.dart
+++ flutter/coder_expenses/lib/components/transaction_list.dart
@@ -10,98 +10,95 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 430,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Text('Sem transações',
-                    style: Theme.of(context).textTheme.headline6),
-                SizedBox(height: 20),
-                Container(
-                    height: 200,
-                    child: Image.asset('assets/images/waiting.png',
-                        fit: BoxFit.cover))
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: FittedBox(
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: Text('R\$${tr.value}'),
-                        ),
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Text('Sem transações',
+                  style: Theme.of(context).textTheme.headline6),
+              SizedBox(height: 20),
+              Container(
+                  height: 200,
+                  child: Image.asset('assets/images/waiting.png',
+                      fit: BoxFit.cover))
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: FittedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Text('R\$${tr.value}'),
                       ),
                     ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: IconButton(
-                      onPressed: () => onRemove(tr.id),
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                    ),
                   ),
-                );
-                // return Card(
-                //   child: Row(children: [
-                //     Container(
-                //       margin: EdgeInsets.symmetric(
-                //         horizontal: 15,
-                //         vertical: 10,
-                //       ),
-                //       decoration: BoxDecoration(
-                //           border: Border.all(
-                //         color: Theme.of(context).colorScheme.primary,
-                //         width: 2,
-                //       )),
-                //       padding: EdgeInsets.all(10),
-                //       child: Text(
-                //         'R\$ ${tr.value.toStringAsFixed(2)}',
-                //         style: TextStyle(
-                //           fontWeight: FontWeight.bold,
-                //           fontSize: 20,
-                //           color: Theme.of(context).colorScheme.primary,
-                //         ),
-                //       ),
-                //     ),
-                //     Column(
-                //       crossAxisAlignment: CrossAxisAlignment.start,
-                //       children: [
-                //         Text(
-                //           tr.title,
-                //           style: TextStyle(
-                //             fontSize: 16,
-                //             fontWeight: FontWeight.bold,
-                //           ),
-                //         ),
-                //         Text(
-                //           DateFormat('d MM y').format(tr.date),
-                //           style: TextStyle(
-                //             color: Colors.grey,
-                //           ),
-                //         )
-                //       ],
-                //     )
-                //   ]),
-                // );
-              },
-            ),
-    );
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(tr.date),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () => onRemove(tr.id),
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                  ),
+                ),
+              );
+              // return Card(
+              //   child: Row(children: [
+              //     Container(
+              //       margin: EdgeInsets.symmetric(
+              //         horizontal: 15,
+              //         vertical: 10,
+              //       ),
+              //       decoration: BoxDecoration(
+              //           border: Border.all(
+              //         color: Theme.of(context).colorScheme.primary,
+              //         width: 2,
+              //       )),
+              //       padding: EdgeInsets.all(10),
+              //       child: Text(
+              //         'R\$ ${tr.value.toStringAsFixed(2)}',
+              //         style: TextStyle(
+              //           fontWeight: FontWeight.bold,
+              //           fontSize: 20,
+              //           color: Theme.of(context).colorScheme.primary,
+              //         ),
+              //       ),
+              //     ),
+              //     Column(
+              //       crossAxisAlignment: CrossAxisAlignment.start,
+              //       children: [
+              //         Text(
+              //           tr.title,
+              //           style: TextStyle(
+              //             fontSize: 16,
+              //             fontWeight: FontWeight.bold,
+              //           ),
+              //         ),
+              //         Text(
+              //           DateFormat('d MM y').format(tr.date),
+              //           style: TextStyle(
+              //             color: Colors.grey,
+              //           ),
+              //         )
+              //       ],
+              //     )
+              //   ]),
+              // );
+            },
+          );
   }
 }
