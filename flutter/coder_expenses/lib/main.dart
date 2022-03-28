f528009cb893e7e3e4718050d58d29da7b767134
Richard Borges <richard.borges@contaazul.com>
2022-03-28 09:40:35 -0300

    feat: understanding text scale factor

--- flutter/coder_expenses/lib/main.dart
+++ flutter/coder_expenses/lib/main.dart
@@ -126,7 +126,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
-      title: Text('Despesas Pessoais'),
+      title: Text(
+        'Despesas Pessoais',
+        style: TextStyle(
+          fontSize: 10 * MediaQuery.of(context).textScaleFactor,
+        ),
+      ),
       actions: <Widget>[
         IconButton(
           icon: Icon(Icons.add),
