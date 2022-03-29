2356e8377635bf2d446b957f22dc7d96d4f93b98
Richard Borges <richard.borges@contaazul.com>
2022-03-29 13:30:26 -0300

    chore: using avalable height in children widgets

--- flutter/coder_expenses/lib/components/chart_bar.dart
+++ flutter/coder_expenses/lib/components/chart_bar.dart
@@ -8,40 +8,47 @@ class ChartBar extends StatelessWidget {
   ChartBar({this.label = '', this.percent = 0.0, this.value = 0.0});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            height: 20,
-            child: FittedBox(child: Text('${value.toStringAsFixed(2)}'))),
-        SizedBox(height: 5),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                      ),
-                      color: Color.fromRGBO(220, 220, 220, 1),
-                      borderRadius: BorderRadius.circular(5))),
-              FractionallySizedBox(
-                heightFactor: percent,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(5)),
-                ),
-              )
-            ],
-          ),
-        ),
-        SizedBox(height: 5),
-        Text(label)
-      ],
+    return LayoutBuilder(
+      builder: (ctx, constrainsts) {
+        return Column(
+          children: [
+            Container(
+                height: constrainsts.maxHeight * 0.15,
+                child: FittedBox(child: Text('${value.toStringAsFixed(2)}'))),
+            SizedBox(height: constrainsts.maxHeight * 0.05),
+            Container(
+              height: constrainsts.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1.0,
+                          ),
+                          color: Color.fromRGBO(220, 220, 220, 1),
+                          borderRadius: BorderRadius.circular(5))),
+                  FractionallySizedBox(
+                    heightFactor: percent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: constrainsts.maxHeight * 0.05),
+            Container(
+              child: FittedBox(child: Text(label)),
+              height: constrainsts.maxHeight * 0.15,
+            )
+          ],
+        );
+      },
     );
   }
 }
