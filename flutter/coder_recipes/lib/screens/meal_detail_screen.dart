5fc0d5a284df7d23a512cd626623932846512f6a
Richard Borges <richard.borges@contaazul.com>
2022-06-06 11:21:46 -0300

    chore: show recipe details

--- flutter/coder_recipes/lib/screens/meal_detail_screen.dart
+++ flutter/coder_recipes/lib/screens/meal_detail_screen.dart
@@ -10,8 +10,52 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Detalhes da Refeição'),
       ),
-      body: Center(
-        child: Text(meal.title),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                  color: Theme.of(context).accentColor,
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
