4639894306e48d2d3b107b8a5a372c5ebd3c3ddb
Richard Borges <richard.borges@contaazul.com>
2022-06-09 11:55:42 -0300

    chore: add bottom navigation bar

--- flutter/coder_recipes/lib/screens/tabs_screen.dart
+++ flutter/coder_recipes/lib/screens/tabs_screen.dart
@@ -2,31 +2,71 @@ import 'package:flutter/material.dart';
 import 'category_screen.dart';
 import 'favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Widget> _screens = [
+    CategoriesScreen(),
+    FavoriteScreen(),
+  ];
+  final List<String> _titles = [
+    'Lista de Categorias',
+    'Meus Favoritos',
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Vamos cozinhar?'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ],
-          ),
+          title: Text(_titles[_selectedScreenIndex]),
+          // bottom: TabBar(
+          //   tabs: [
+          //     Tab(
+          //       icon: Icon(Icons.category),
+          //       text: 'Categorias',
+          //     ),
+          //     Tab(
+          //       icon: Icon(Icons.star),
+          //       text: 'Favoritos',
+          //     ),
+          //   ],
+          // ),
         ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
+        // body: TabBarView(
+        //   children: [
+        //     CategoriesScreen(),
+        //     FavoriteScreen(),
+        //   ],
+        // ),
+        body: _screens[_selectedScreenIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedScreenIndex,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favoritos',
+            ),
           ],
         ),
       ),
