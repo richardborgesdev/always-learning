bac445d6406d4584b02c2fe0becd45a97238189b
Richard Borges <richard.borges@contaazul.com>
2022-08-08 11:08:57 -0300

    chore: using change notifier provide on product list

--- flutter/coder_shop/lib/pages/products_overview_page.dart
+++ flutter/coder_shop/lib/pages/products_overview_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/ProductList.dart';
 import '../models/product.dart';
 import '../components/product_item.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
