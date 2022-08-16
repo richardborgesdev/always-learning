b0bcc097731056fa51916fbd56a8b860fde88227
Richard Borges <richard.borges@contaazul.com>
2022-08-16 13:47:24 -0300

    chore: use consumer to optimize render

--- flutter/coder_shop/lib/main.dart
+++ flutter/coder_shop/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/product_list.dart';
-import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
-import 'providers/counter.dart';
 
 void main() {
   runApp(MyApp());
