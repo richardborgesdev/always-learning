8ae0321d4e6deb236e97a56117dff2eeb5e42ac6
Richard Borges <richard.borges@contaazul.com>
2022-08-17 09:45:24 -0300

    chore: filtering global product list with provider

--- flutter/coder_shop/lib/pages/products_overview_page.dart
+++ flutter/coder_shop/lib/pages/products_overview_page.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import '../components/product_grid.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text(
+                  'Somente favoritos',
+                ),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text(
+                  'Todos',
+                ),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: ProductGrid(),
     );
