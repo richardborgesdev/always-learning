d08d2b8d528d645accaa610ec0835d90e3fe2bbc
Richard Borges <richard.borges@contaazul.com>
2022-09-06 11:54:40 -0300

    chore: show product details

--- flutter/coder_shop/lib/pages/product_detail_page.dart
+++ flutter/coder_shop/lib/pages/product_detail_page.dart
@@ -17,6 +17,43 @@ class ProductDetailPage extends StatelessWidget {
           product.name,
         ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'R\$ ${product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
