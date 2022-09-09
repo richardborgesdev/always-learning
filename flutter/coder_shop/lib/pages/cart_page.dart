581804ef17177ff48abd078cdfeb57e3c0930938
Richard Borges <richard.borges@contaazul.com>
2022-09-09 10:07:17 -0300

    chore: add order

--- flutter/coder_shop/lib/pages/cart_page.dart
+++ flutter/coder_shop/lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models/order_list.dart';
 
 import '../components/cart_item.dart';
 import '../models/cart.dart';
@@ -53,7 +54,14 @@ class CartPage extends StatelessWidget {
                   ),
                   Spacer(),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<OrderList>(
+                        context,
+                        listen: false,
+                      ).addOrder(cart);
+
+                      cart.clear();
+                    },
                     child: Text(
                       'COMPRAR',
                     ),
