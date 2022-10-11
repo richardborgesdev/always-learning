18b774640b1d0c9c741c5d15e092230d41eba0ef
richard borges <richard.borges.cc@gmail.com>
2022-10-11 10:05:18 -0300

    chore: refactor submit data

--- flutter/coder_shop/lib/pages/product_form_page.dart
+++ flutter/coder_shop/lib/pages/product_form_page.dart
@@ -1,7 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:shop/models/product.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({Key? key}) : super(key: key);
@@ -56,13 +57,10 @@ class _ProductFormPageState extends State<ProductFormPage> {
 
     _formKey.currentState?.save();
 
-    final newProduct = Product(
-      id: Random().nextDouble().toString(),
-      name: _formData['name'] as String,
-      description: _formData['description'] as String,
-      price: _formData['price'] as double,
-      imageUrl: _formData['imageUrl'] as String,
-    );
+    Provider.of<ProductList>(context, listen: false)
+        .addProductFromData(_formData);
+
+    Navigator.of(context).pop();
   }
 
   @override
