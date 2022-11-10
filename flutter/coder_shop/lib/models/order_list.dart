6a865845c8d5438c44101fd20c6cf9c48a855f58
richard borges <richard.borges.cc@gmail.com>
2022-11-10 11:46:42 -0300

    chore: send order to backend

--- flutter/coder_shop/lib/models/order_list.dart
+++ flutter/coder_shop/lib/models/order_list.dart
@@ -1,8 +1,10 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/models/order.dart';
 
+import '../utils/constants.dart';
 import 'cart.dart';
 
 class OrderList with ChangeNotifier {
@@ -16,14 +18,33 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final postAddOrderResponse = await http.post(
+      Uri.parse('${Constants.ORDERS_BASE_URL}.json'),
+      body: jsonEncode({
+        "total": cart.totalAmount,
+        "date": date.toIso8601String(),
+        "products": cart.items.values
+            .map((cartItem) => {
+                  "id": cartItem.id,
+                  "productId": cartItem.productId,
+                  "name": cartItem.name,
+                  "quantity": cartItem.quantity,
+                  "price": cartItem.price,
+                })
+            .toList(),
+      }),
+    );
+
+    final id = jsonDecode(postAddOrderResponse.body)['name'];
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount,
         products: cart.items.values.toList(),
-        date: DateTime.now(),
+        date: date,
       ),
     );
 
--- flutter/coder_shop/lib/utils/constants.dart
+++ flutter/coder_shop/lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const PRODUCT_BASE_URL =
       'https://addDBId-default-rtdb.firebaseio.com/products';
+  static const ORDERS_BASE_URL =
+      'https://addDBId-default-rtdb.firebaseio.com/orders';
 }
