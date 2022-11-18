e036dc63b8575bef9c543249aebab86ce0c5fefa
richard borges <richard.borges.cc@gmail.com>
2022-11-18 09:40:53 -0300

    chore: styling auth page

--- flutter/coder_shop/lib/pages/auth_page.dart
+++ flutter/coder_shop/lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -15,13 +17,50 @@ class AuthPage extends StatelessWidget {
                   Color.fromRGBO(215, 117, 255, 0.5),
                   Color.fromRGBO(215, 117, 255, 0.9),
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
           Container(
             width: double.infinity,
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  // cascade operator ..
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        20,
+                      ),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(
+                            0,
+                            2,
+                          ),
+                        )
+                      ]),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color:
+                          Theme.of(context).accentTextTheme?.headline6?.color,
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ],
