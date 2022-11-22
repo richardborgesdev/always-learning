6269ec874857249c08a6cf0b633dd4212c13186b
richard borges <richard.borges.cc@gmail.com>
2022-11-22 14:53:13 -0300

    chore: sign up form validation

--- flutter/coder_shop/lib/components/auth_form.dart
+++ flutter/coder_shop/lib/components/auth_form.dart
@@ -14,6 +14,11 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   AuthMode _authMode = AuthMode.Login;
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +47,16 @@ class _AuthFormState extends State<AuthForm> {
                 labelText: 'E-mail',
               ),
               keyboardType: TextInputType.emailAddress,
+              onSaved: (email) => _authData['email'] = email ?? '',
+              validator: (_email) {
+                final email = _email ?? '';
+
+                if (email.trim().isEmpty || !email.contains('@')) {
+                  return 'Informe um e-mail válido.';
+                }
+
+                return null;
+              },
             ),
             TextFormField(
               decoration: InputDecoration(
@@ -49,6 +64,17 @@ class _AuthFormState extends State<AuthForm> {
               ),
               keyboardType: TextInputType.emailAddress,
               obscureText: true,
+              controller: _passwordController,
+              onSaved: (password) => _authData['password'] = password ?? '',
+              validator: (_password) {
+                final password = _password ?? '';
+
+                if (password.trim().isEmpty || password.length < 5) {
+                  return 'Informe uma senha válida.';
+                }
+
+                return null;
+              },
             ),
             if (_authMode == AuthMode.Login)
               TextFormField(
@@ -57,6 +83,15 @@ class _AuthFormState extends State<AuthForm> {
                 ),
                 keyboardType: TextInputType.emailAddress,
                 obscureText: true,
+                validator: (_password) {
+                  final password = _password ?? '';
+
+                  if (password != _passwordController.text) {
+                    return 'Senhas informadas não conferem.';
+                  }
+
+                  return null;
+                },
               ),
             SizedBox(
               height: 20,
