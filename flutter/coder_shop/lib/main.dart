fc327fa8cf98bd54f6564c64a25dc1e21f852829
richard borges <richard.borges.cc@gmail.com>
2022-11-30 15:41:59 -0300

    chore: handle provider dependencies

--- flutter/coder_shop/lib/main.dart
+++ flutter/coder_shop/lib/main.dart
@@ -25,16 +25,22 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => ProductList(),
+          create: (_) => Auth(),
+        ),
+        ChangeNotifierProxyProvider<Auth, ProductList>(
+          create: (_) => ProductList('', []),
+          update: (context, auth, previous) {
+            return ProductList(
+              auth.token ?? '',
+              previous?.items ?? [],
+            );
+          },
         ),
         ChangeNotifierProvider(
           create: (_) => Cart(),
         ),
         ChangeNotifierProvider(
           create: (_) => OrderList(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => Auth(),
         ),
       ],
       child: MaterialApp(
