ae2ae83d729d20621a8114427cf49df7b8e9ed45
richard borges <richard.borges.cc@gmail.com>
2022-12-20 11:03:26 -0300

    chore: use animated container instead of animated builder

--- flutter/coder_shop/lib/components/auth_form.dart
+++ flutter/coder_shop/lib/components/auth_form.dart
@@ -143,16 +143,16 @@ class _AuthFormState extends State<AuthForm>
           10,
         ),
       ),
-      child: AnimatedBuilder(
-        animation: _heightAnimation!,
-        builder: (context, childForm) => Container(
-          padding: const EdgeInsets.all(
-            16,
-          ),
-          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-          width: deviceSize.width * 0.75,
-          child: childForm,
+      child: AnimatedContainer(
+        duration: Duration(
+          milliseconds: 300,
         ),
+        curve: Curves.linear,
+        padding: const EdgeInsets.all(
+          16,
+        ),
+        height: _isLogin() ? 310 : 400,
+        width: deviceSize.width * 0.75,
         child: Form(
           key: _formKey,
           child: Column(
