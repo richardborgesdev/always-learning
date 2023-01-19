59299ab9695331e9a449c9462c68cf6db2193fef
richard borges <richard.borges.cc@gmail.com>
2023-01-19 14:00:05 -0300

    chore: route transition animation

--- flutter/coder_shop/lib/components/app_drawer.dart
+++ flutter/coder_shop/lib/components/app_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 import '../models/auth.dart';
 
@@ -41,8 +43,13 @@ class AppDrawer extends StatelessWidget {
               'Pedidos',
             ),
             onTap: () {
-              Navigator.of(context).pushReplacementNamed(
-                AppRoutes.ORDERS,
+              // Navigator.of(context).pushReplacementNamed(
+              //   AppRoutes.ORDERS,
+              // );
+              Navigator.of(context).pushReplacement(
+                CustomRoute(
+                  builder: (ctx) => OrdersPage(),
+                ),
               );
             },
           ),
